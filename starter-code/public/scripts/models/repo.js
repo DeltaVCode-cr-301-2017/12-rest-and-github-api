9ebfc08b6c856e3831b949a35916514102ec39e9
ByteMiBro <bytemibro@gmail.com>
2017-07-25 14:22:16 -0500

    added callback to requestrepos

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,8 +6,8 @@ var app = app || {};
 
   repos.all = [];
 
-  repos.requestRepos = function(/*callback*/) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+  repos.requestRepos = function(callback) {
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
@@ -20,11 +20,10 @@ var app = app || {};
       // render the data
         data => {
           data.forEach(repo => repos.all.push(repo));
+          callback();
           console.log(repos.all);
         }
       );
-
-    /*callback();*/
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
